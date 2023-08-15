db2dae66311f1b45ae47d5e3569347ec152e44da
Antropov Anton <antropov.a.g@gmail.com>
2023-08-15 20:47:17 +0400

    Add universal function

--- pages/index.js
+++ pages/index.js
@@ -36,11 +36,10 @@ const time = document.querySelector("#time");
 const cost = document.querySelector("#cost");
 const navigationDots = document.querySelectorAll(".completed-projects__radio-button");
 const navigationLinks = document.querySelectorAll(".completed-projects__link");
-console.log(navigationLinks)
 
 let currentIndex = 0;
 
-function rednerInitialCard (arr) {
+function rednerCards (arr) {
   image.src = arr[currentIndex].image;
   image.alt = arr[currentIndex].alt;
   city.innerHTML = arr[currentIndex].city;
@@ -48,54 +47,56 @@ function rednerInitialCard (arr) {
   time.innerHTML = arr[currentIndex].time;
   cost.innerHTML = arr[currentIndex].cost;
   navigationDots[currentIndex].classList.add("completed-projects__radio-button_type_selected")
+  navigationLinks[currentIndex].classList.add("completed-projects__link_type_selected");
 }
 
-rednerInitialCard(cardsInfo);
+rednerCards(cardsInfo);
+
+function renderCard (item) {
+  navigationLinks[currentIndex].classList.remove("completed-projects__link_type_selected");
+  navigationDots[currentIndex].classList.remove("completed-projects__radio-button_type_selected")
+  currentIndex = Number(item.dataset.index);
+  image.src = cardsInfo[currentIndex].image;
+  image.alt = cardsInfo[currentIndex].alt;
+  city.innerHTML = cardsInfo[currentIndex].city;
+  area.innerHTML = cardsInfo[currentIndex].area;
+  time.innerHTML = cardsInfo[currentIndex].time;
+  cost.innerHTML = cardsInfo[currentIndex].cost;
+  navigationDots[currentIndex].classList.add("completed-projects__radio-button_type_selected");
+  navigationLinks[currentIndex].classList.add("completed-projects__link_type_selected");
+  return currentIndex;
+}
+
+navigationDots.forEach((item) => {
+  item.addEventListener("click", () => {
+    renderCard(item)
+  })
+});
+
+navigationLinks.forEach(item => {
+  item.addEventListener("click", () => {
+    renderCard(item)
+  })
+})
 
 leftButton.addEventListener('click', () => {
+    navigationLinks[currentIndex].classList.remove("completed-projects__link_type_selected");
     navigationDots[currentIndex].classList.remove("completed-projects__radio-button_type_selected")
     if(currentIndex <= 0) {
     currentIndex = cardsInfo.length - 1;
     } else {
     currentIndex = currentIndex - 1;
 }
-    image.src = cardsInfo[currentIndex].image;
-    image.alt = cardsInfo[currentIndex].alt;
-    city.innerHTML = cardsInfo[currentIndex].city;
-    area.innerHTML = cardsInfo[currentIndex].area;
-    time.innerHTML = cardsInfo[currentIndex].time;
-    cost.innerHTML = cardsInfo[currentIndex].cost;
-    navigationDots[currentIndex].classList.add("completed-projects__radio-button_type_selected")
+    rednerCards(cardsInfo);
 });
 
 rightButton.addEventListener('click', () => {
+    navigationLinks[currentIndex].classList.remove("completed-projects__link_type_selected");
     navigationDots[currentIndex].classList.remove("completed-projects__radio-button_type_selected")
     if(currentIndex >= (cardsInfo.length - 1)) {
     currentIndex = 0;
     } else {
     currentIndex = currentIndex + 1;
 }
-    image.src = cardsInfo[currentIndex].image;
-    image.alt = cardsInfo[currentIndex].alt;
-    city.innerHTML = cardsInfo[currentIndex].city;
-    area.innerHTML = cardsInfo[currentIndex].area;
-    time.innerHTML = cardsInfo[currentIndex].time;
-    cost.innerHTML = cardsInfo[currentIndex].cost;
-    navigationDots[currentIndex].classList.add("completed-projects__radio-button_type_selected")
+    rednerCards(cardsInfo);
 });
-
-navigationDots.forEach((item) => {
-  item.addEventListener("click", () => {
-    navigationDots[currentIndex].classList.remove("completed-projects__radio-button_type_selected")
-    currentIndex = item.id;
-    image.src = cardsInfo[currentIndex].image;
-    image.alt = cardsInfo[currentIndex].alt;
-    city.innerHTML = cardsInfo[currentIndex].city;
-    area.innerHTML = cardsInfo[currentIndex].area;
-    time.innerHTML = cardsInfo[currentIndex].time;
-    cost.innerHTML = cardsInfo[currentIndex].cost;
-    navigationDots[currentIndex].classList.add("completed-projects__radio-button_type_selected")
-    return currentIndex;
-  })
-})
-
